608ed68f70571cf4323aabc9302bc6a8b7e822db
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2024-08-13 19:05:36 -0300

    initial setup

---
+++ FigmaChallenge/FigmaChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FigmaChallenge
+//
+//  Created by Débora Lage on 13/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FigmaChallenge/FigmaChallenge/FigmaChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaChallengeApp.swift
+//  FigmaChallenge
+//
+//  Created by Débora Lage on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FigmaChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
