b6af918ca8858f965ba8be64f1ccd0f61ad01258
Débora Lage <debora.barbosa@aluno.ufop.edu.br>
2024-08-20 23:17:40 -0300

    add initial buttons

--- Wallet/Wallet/ContentView.swift
+++ Wallet/Wallet/ContentView.swift
@@ -9,13 +9,67 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            
+            VStack {
+                buttonsView
+                myCardsView
+                carouselCard()
+            }
+            .padding(40)
+            
         }
-        .padding()
+    }
+    
+    var buttonsView: some View {
+        HStack {
+            circularButton(image: "circle.grid.3x3.fill") { }
+            Spacer()
+            circularButton(image: "bell") { }
+        }
+    }
+    
+    var myCardsView: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("My")
+                    .foregroundStyle(.white.opacity(0.55))
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Text("Cards")
+                    .foregroundStyle(.white)
+                    .font(.title)
+                    .fontWeight(.bold)
+            }
+            
+            
+            Spacer()
+            circularButton(image: "plus") { }
+        }
+    }
+    
+    @ViewBuilder
+    func circularButton(image: String, action: () -> Void) -> some View {
+        Button {
+
+        } label: {
+            Image(systemName: image)
+                .renderingMode(.template)
+                .resizable()
+                .foregroundStyle(.white)
+                .frame(width: 22, height: 22)
+        }
+        .frame(width: 54, height: 54)
+        .background(Color.black.opacity(0.05))
+        .clipShape(Circle())
+    }
+    
+    @ViewBuilder
+    func carouselCard() -> some View {
+        EmptyView()
     }
 }
 
